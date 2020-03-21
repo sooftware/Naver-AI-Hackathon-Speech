45fdb2ad7050332f09a18930b1f321b6221cd5f9
KimSooHwan <sh951011@gmail.com>
2020-03-22 01:57:58 +0900

    Clean Code

--- models/DecoderRNN.py
+++ models/DecoderRNN.py
@@ -21,7 +21,6 @@ import torch.nn as nn
 from torch.autograd import Variable
 import torch.nn.functional as F
 
-from models.Beam import Beam
 from .attention import Attention
 from .baseRNN import BaseRNN
 
@@ -32,7 +31,7 @@ else:
 
 
 class DecoderRNN(BaseRNN):
-    """
+    r"""
     Provides functionality for decoding in a seq2seq framework, with an option for attention.
     Args:
         vocab_size (int): size of the vocabulary
@@ -46,10 +45,8 @@ class DecoderRNN(BaseRNN):
         input_dropout_p (float, optional): dropout probability for the input sequence (default: 0)
         dropout_p (float, optional): dropout probability for the output sequence (default: 0)
         use_attention(bool, optional): flag indication whether to use attention mechanism or not (default: false)
-    Attributes:
-        KEY_ATTN_SCORE (str): key used to indicate attention weights in `ret_dict`
-        KEY_LENGTH (str): key used to indicate a list representing lengths of output sequences in `ret_dict`
-        KEY_SEQUENCE (str): key used to indicate a list of sequences in `ret_dict`
+
+
     Inputs: inputs, encoder_hidden, encoder_outputs, function, teacher_forcing_ratio
         - **inputs** (batch, seq_len, input_size): list of sequences, whose length is the batch size and within which
           each sequence is a list of token IDs.  It is used for teacher forcing when provided. (default `None`)
@@ -62,6 +59,7 @@ class DecoderRNN(BaseRNN):
         - **teacher_forcing_ratio** (float): The probability that teacher forcing will be used. A random number is
           drawn uniformly from 0-1 for every decoding token, and if the sample is smaller than the given value,
           teacher forcing would be used (default is 0).
+
     Outputs: decoder_outputs, decoder_hidden, ret_dict
         - **decoder_outputs** (seq_len, batch, vocab_size): list of tensors with size (batch_size, vocab_size) containing
           the outputs of the decoding function.
@@ -75,8 +73,7 @@ class DecoderRNN(BaseRNN):
     def __init__(self, vocab_size, max_len, hidden_size,
             sos_id, eos_id,
             n_layers=1, rnn_cell='gru', bidirectional=False,
-            input_dropout_p=0, dropout_p=0, use_attention=False,
-            use_beam_search = False):
+            input_dropout_p=0, dropout_p=0, use_attention=False):
         super(DecoderRNN, self).__init__(vocab_size, max_len, hidden_size,
                 input_dropout_p, dropout_p,
                 n_layers, rnn_cell)
@@ -84,6 +81,7 @@ class DecoderRNN(BaseRNN):
         self.bidirectional_encoder = bidirectional
         # rnn_cell -> gru를 의미
         self.rnn = self.rnn_cell(hidden_size , hidden_size, n_layers, batch_first=True, dropout=dropout_p)
+
         self.output_size = vocab_size # 발음 개수 사이즈만큼 output을 내놓아야 하니까!!
         self.max_length = max_len
         self.use_attention = use_attention
@@ -95,47 +93,68 @@ class DecoderRNN(BaseRNN):
         if use_attention:
             self.attention = Attention(self.hidden_size)
         self.out = nn.Linear(self.hidden_size, self.output_size)
-        self.use_beam_search = use_beam_search
-        self.beam_width = 1
 
     def forward_step(self, input_var, hidden, encoder_outputs, function):
         batch_size = input_var.size(0)
         output_size = input_var.size(1)
+
         embedded = self.embedding(input_var)
         embedded = self.input_dropout(embedded)
+
         if self.training:
             self.rnn.flatten_parameters()
 
         # hidden_size, output_size,
-        output, hidden = self.rnn(embedded, hidden) # 여기서 문제가 생김
+        output, hidden = self.rnn(embedded, hidden)
         attn = None
+
         if self.use_attention:
-            output, attn = self.attention(output, encoder_outputs)  # 여기 수정
+            output, attn = self.attention(output, encoder_outputs)
+
         predicted_softmax = function(self.out(output.contiguous().view(-1, self.hidden_size)), dim=1).view(batch_size, output_size, -1)
         return predicted_softmax, hidden, attn
 
-    def forward(self, inputs=None, encoder_hidden=None, encoder_outputs=None,
-                    function=F.log_softmax, teacher_forcing_ratio=0):
+    def forward(self, inputs=None, encoder_hidden=None, encoder_outputs=None, function=F.log_softmax, teacher_forcing_ratio=0):
 
-        inputs, batch_size, max_length = self._validate_args(inputs, encoder_hidden, encoder_outputs,
-                                                             function, teacher_forcing_ratio)
-        # (layer_size, batch_size, hidden_size)
+        inputs, batch_size, max_length = self._validate_args(
+            inputs,
+            encoder_hidden,
+            encoder_outputs,
+            function,
+            teacher_forcing_ratio
+        )
         decoder_hidden = self._init_state(encoder_hidden)
         use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
 
         decoder_outputs = []
+        sequence_symbols = []
+        lengths = np.array([max_length] * batch_size)
 
         def decode(step, step_output, step_attn):
             decoder_outputs.append(step_output)
+
             symbols = decoder_outputs[-1].topk(1)[1]
+            sequence_symbols.append(symbols)
+
+            eos_batches = symbols.data.eq(self.eos_id)
+            if eos_batches.dim() > 0:
+                eos_batches = eos_batches.cpu().view(-1).numpy()
+                update_idx = ((lengths > step) & eos_batches) != 0
+                lengths[update_idx] = len(sequence_symbols)
+
             return symbols
 
         # Manual unrolling is used to support random teacher forcing.
         # If teacher_forcing_ratio is True or False instead of a probability, the unrolling can be done in graph
         if use_teacher_forcing:
             decoder_input = inputs[:, :-1]
-            decoder_output, decoder_hidden, attn = self.forward_step(decoder_input, decoder_hidden, encoder_outputs,
-                                                                     function=function)
+            decoder_output, decoder_hidden, attn = self.forward_step(
+                decoder_input,
+                decoder_hidden,
+                encoder_outputs,
+                function=function
+            )
+
             for di in range(decoder_output.size(1)):
                 step_output = decoder_output[:, di, :]
                 if attn is not None:
@@ -144,37 +163,17 @@ class DecoderRNN(BaseRNN):
                     step_attn = None
                 decode(di, step_output, step_attn)
         else:
-
-            #########################
-            #########################
-            #########################
-
-            if self.use_beam_search :
-                beam_list = []
-                for i in range(self.beam_width):
-                    beam_list.append(Beam(self.eos_id))
-
-                decoder_input = inputs[:, 0].unsqueeze(1)
-                for di in range(max_length):
-                    decoder_output, decoder_hidden, step_attn = self.forward_step(decoder_input, decoder_hidden, encoder_outputs, function=function)
-                    step_output = decoder_output.squeeze(1)
-                    decoder_outputs.append(step_output)
-
-
-            #########################
-            #########################
-            #########################
-
-
-            else :
-                decoder_input = inputs[:, 0].unsqueeze(1)
-                for di in range(max_length):
-                    decoder_output, decoder_hidden, step_attn = self.forward_step(decoder_input, decoder_hidden, encoder_outputs,
-                                                                             function=function)
-                    step_output = decoder_output.squeeze(1)
-                    decoder_outputs.append(step_output)
-                    symbols = decoder_outputs[-1].topk(3)[1]
-                    decoder_input = symbols
+            decoder_input = inputs[:, 0].unsqueeze(1)
+            for di in range(max_length):
+                decoder_output, decoder_hidden, step_attn = self.forward_step(
+                    decoder_input,
+                    decoder_hidden,
+                    encoder_outputs,
+                    function=function
+                )
+                step_output = decoder_output.squeeze(1)
+                symbols = decode(di, step_output, step_attn)
+                decoder_input = symbols
 
         return decoder_outputs, decoder_hidden
 
